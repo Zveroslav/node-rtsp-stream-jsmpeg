d52abad9ff69a427a4b8fe0e723626ce89b2e8e2
AndreaMontalbani <andreamontalbaniwork@gmail.com>
2024-08-12 01:19:58 +0200

    Fixed "server.js" test script, passing option "wsPort" instead of "port" previously throwing error. Refer to issue #15

--- test/server.js
+++ test/server.js
@@ -3,7 +3,7 @@ const Stream = require('../src/videoStream')
 const options = {
   name: 'streamName',
   url: 'rtsp://184.72.239.149/vod/mp4:BigBuckBunny_115k.mov',
-  port: 5000
+  wsPort: 5000
 }
 
 stream = new Stream(options)
